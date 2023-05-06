fe2bc3f03b5b926f99a34235f91c0ed46007cde0
Enzo <enzocasamasso@gmail.com>
2023-05-05 21:50:20 -0300

    first files and configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'yoozi-order-view';
+  title = signal<string>("Yoozi Order")
 }
